f99db310c8a31cc0f31558ef4dd77ad243178bc9
Tushar <tusharkhandelwal1512@gmail.com>
2024-01-17 15:23:55 +0530

    app setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
+      fontFamily:{
+        satoshi : ['satoshi', 'sans-serif'],
+        inter : ['inter', 'sans-serif'],
       },
+      colors:{
+        'primary-orange': '#FF5722'
+      }
     },
   },
   plugins: [],
