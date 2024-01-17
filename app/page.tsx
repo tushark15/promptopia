ae0db2e7935ab134dc6e5e64bdc834471ae4add5
Tushar <tusharkhandelwal1512@gmail.com>
2024-01-17 16:58:43 +0530

    added basic headers and para to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 const Home = () => {
   return (
-    <div>page</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and share
+        <br/>
+        <span className="orange_gradient text-center">AI Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is a community of writers who share and discover AI generated
+      </p>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
